4b0f7ba02dadf96b46bf12181cad6c8229789abd
root <root@localhost.localdomain>
2016-07-26 22:15:40 +0800

    Rename the blog title and hide the personal blogs

--- localTest.sh
+++ localTest.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
 
-if [[ $# -lt 1 ]]; then
+# For osx
+#if [[ $# -lt 1 ]]; then
+#    echo "Usage:"
+#    echo "      r to run the local server"
+#    echo "      c to clean the local _site folder"
+#    exit
+#fi
+#
+#if [[ $1 = "r" ]]; then
+#    jekyll s -w
+#elif [[ $1 = "c" ]]; then
+#    rm -fr _site
+#fi
+
+# For linux
+if [ $# -lt 1 ]; then
     echo "Usage:"
     echo "      r to run the local server"
     echo "      c to clean the local _site folder"
     exit
 fi
 
-if [[ $1 = "r" ]]; then
+if [ $1 = "r" ]; then
     jekyll s -w
-elif [[ $1 = "c" ]]; then
+elif [ $1 = "c" ]; then
     rm -fr _site
 fi
